dcaad62353d241caf0be26c39fbfb22075f7a66a
Vytautas-T <105976888+Vytautas-T@users.noreply.github.com>
2022-06-13 22:19:47 +0300

    papildoma uzduotis nezinau ar teisingiai padaryta

---
+++ papildoma uzduotis 1/script.js
@@ -0,0 +1,34 @@
+window.onload = function() {
+
+    //console.log(generateRandom())
+
+    //let remdomcount = generateRandom;
+    let a = 15749;
+    let b = 95000;
+    let x = 39857;
+    console.log(skaiciuSudejimas(generateRandom(),b))
+    console.log(generateRandom())
+}
+
+function generateRandom(min = 10000, max = 99999) {
+
+    // find diff
+    let difference = max - min;
+
+    // generate random number 
+    let rand = Math.random();
+
+    // multiply with difference 
+    rand = Math.floor( rand * difference);
+
+    // add with min value 
+    rand = rand + min;
+
+    return rand;
+}
+
+function skaiciuSudejimas (skaicius,skaicius2) {
+     let skaicius3 = skaicius + skaicius2;
+     return skaicius3.toString().split("").length;
+
+}
